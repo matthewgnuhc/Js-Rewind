c7c127dad1c0f1305f28880b20619f5118c9bac2
Matthew Chung <matthewgnuhc@gmail.com>
2021-10-05 11:40:46 -0700

    init commit

--- js/site.js
+++ js/site.js
@@ -1,20 +1,42 @@
+//Get starting values from the screen
+//Controller Function
+function getString() {
+    //Get the user's input
+    let userString = document.getElementById("userString").value;
+    //Version 2 --Test the user's input for length
 
-// Displays a message to the user
-function displayMessage() {
-    // Declare our variable
-    let msg = "";
-    // Pull the value from the input box
-    msg = document.getElementById("message").value;
-    // Alert the user
-    alert(msg);
+    //Reverse the user's input - this is a seperate function
+    let revString = reverseString(userString);
+
+    //Display the result - this is a seperate function
+    displayString(revString)
 }
 
-function displayMessage2() {
-    
-    let msg = "";
-    
-    msg = document.getElementById("message").value;
-    
-    Swal.fire(msg);
+function reverseString(userString) {
+    let startValue = userString.length - 1;
+    let endValue = 0;
+
+    let revString = "";
+
+    //User input = Frank
+    //Frank is a string AND an array of characters
+    //[F, r, a, n, k]
+    //In a decremented for loop the startValue must be higher than the endValue.
+    for (let i = startValue; i >= endValue; i--){
+        revString += userString[i];
+    }
+
+    //loop 1 -"k"
+    //loop 2 - "kn"
+    //loop 3 - "kna"
+    //loop 4 - "knar"
+    //loop 5 - "knarF"
+    return revString;
+
 }
 
+function displayString(revString) {
+    let output = document.getElementById("results");
+
+    output.innerHTML = revString;
+}
